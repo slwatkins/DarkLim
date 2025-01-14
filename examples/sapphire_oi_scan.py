Fixed units on LEE rate, and made plots nicer

examples/sapphire_oi_scan.py
@@ -59,10 +59,10 @@ def process_mass(mass, args):
 
     SE = darklim.sensitivity.SensEst(args.target_mass_kg, args.t_days, tm=args.target, eff=1., gain=1., seed=(int(time.time() + mass*1e6)))
     SE.reset_sim()
-    #SE.add_nfold_lee_bkgd(m=args.n_sensors, n=args.coincidence, w=args.window_s, e0=0.41e-3, R=33.)
-    #SE.add_nfold_lee_bkgd(m=args.n_sensors, n=args.coincidence, w=args.window_s, e0=3.81e-3, R=0.0226)
+    SE.add_nfold_lee_bkgd(m=args.n_sensors, n=args.coincidence, w=args.window_s, e0=0.41e-3, R=33.)
+    SE.add_nfold_lee_bkgd(m=args.n_sensors, n=args.coincidence, w=args.window_s, e0=3.81e-3, R=0.023)
     SE.add_power_bkgd(1.4e-8, 5.77)
-    SE.add_power_bkgd(7.51e-11, 2.72)
+    SE.add_power_bkgd(0.107, 2.72)
 
     per_device_threshold_keV = args.nsigma * args.baseline_res_eV * 1e-3
     threshold_keV = args.coincidence * per_device_threshold_keV
@@ -103,8 +103,11 @@ def sapphire_scan():
     scanparser.write_info(args)
 
     # Main parallel execution block
-    with mp.Pool(processes=min(args.max_cpus, mp.cpu_count())) as pool:
-        results = pool.starmap(process_mass, [(mass, args) for mass in args.masses_GeV])
+#    with mp.Pool(processes=min(args.max_cpus, mp.cpu_count())) as pool:
+#        results = pool.starmap(process_mass, [(mass, args) for mass in args.masses_GeV])
+    results = []
+    for i, mass in enumerate(args.masses_GeV):
+        results.append(process_mass(mass, args))
 
     # save results to txt file
     sigma = np.zeros_like(args.masses_GeV)